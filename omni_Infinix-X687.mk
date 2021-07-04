2e6f29bfb1be2757d7b480e444dcc75c9a3abbbe
Vergil Pampanga <83948563+mastersenpai05@users.noreply.github.com>
2021-07-04 17:36:40 +0800

    change format

--- omni_Infinix-X687.mk
+++ omni_Infinix-X687.mk
@@ -16,23 +16,22 @@
 # limitations under the License.
 #
 
+LOCAL_PATH := device/infinix/X687
+
+# Dynamic
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-
-# Inherit from Infinix-X687 device
-$(call inherit-product, device/infinix/Infinix-X687/device.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
-$(call inherit-product, vendor/omni/config/gsm.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := Infinix-X687
-PRODUCT_NAME := omni_Infinix-X687
+PRODUCT_DEVICE := X687
+PRODUCT_NAME := omni_X687
 PRODUCT_BRAND := Infinix
-PRODUCT_MODEL := Infinix
+PRODUCT_MODEL := X687
 PRODUCT_MANUFACTURER := infinix
-PRODUCT_RELEASE_NAME := Infinix Infinix
+PRODUCT_RELEASE_NAME := X687
